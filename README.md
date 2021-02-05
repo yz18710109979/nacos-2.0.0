d6f888f3090bff3ce2e9dcda562f4ef74f47c101
“e <1922375530@qq.com>
2021-02-05 14:11:43 +0800

    模块注释

--- README.md
+++ README.md
@@ -4,4 +4,19 @@ SpringCloud Nacos源码分析
 1.启动类（console） com.alibaba.nacos.Nacos
 增加启动参数
     -Dnacos.standalone=true -Dnacos.home=C:\nacos
+
 # nacos-2.0.0
+
+
+
+
+
+```
+echo "# springcloud" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yz18710109979/springcloud.git
+git push -u origin main
+```
--- core/src/main/java/com/alibaba/nacos/core/code/StandaloneProfileApplicationListener.java
+++ core/src/main/java/com/alibaba/nacos/core/code/StandaloneProfileApplicationListener.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 1999-2018 Alibaba Group Holding Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.alibaba.nacos.core.code;
 
 import com.alibaba.nacos.sys.env.EnvUtil;
@@ -39,25 +23,25 @@ import static com.alibaba.nacos.sys.env.Constants.STANDALONE_SPRING_PROFILE;
  */
 public class StandaloneProfileApplicationListener
         implements ApplicationListener<ApplicationEnvironmentPreparedEvent>, PriorityOrdered {
-    
+
     private static final Logger LOGGER = LoggerFactory.getLogger(StandaloneProfileApplicationListener.class);
-    
+
     @Override
     public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
-        
+
         ConfigurableEnvironment environment = event.getEnvironment();
 
         if (environment.getProperty(STANDALONE_MODE_PROPERTY_NAME, boolean.class, false)) {
             environment.addActiveProfile(STANDALONE_SPRING_PROFILE);
         }
-        
+
         if (LOGGER.isInfoEnabled()) {
             LOGGER.info("Spring Environment's active profiles : {} in standalone mode : {}",
                     Arrays.asList(environment.getActiveProfiles()), EnvUtil.getStandaloneMode());
         }
-        
+
     }
-    
+
     @Override
     public int getOrder() {
         return HIGHEST_PRECEDENCE;
